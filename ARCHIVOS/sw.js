7b5827645a940d279e6aca0136ccc610e27a9cd0
Laidel <51295036+Laidel@users.noreply.github.com>
2026-04-12 09:23:18 -0400

    Update sw.js

--- ARCHIVOS/sw.js
+++ ARCHIVOS/sw.js
@@ -1,24 +1,33 @@
-const CACHE_NAME = 'florida-v1';
-const ASSETS = [
-    './TiendaVirtual.html',
-    'https://img.icons8.com/color/48/mame.png',
-    'https://img.icons8.com/color/48/super-nintendo.png',
-    'https://img.icons8.com/color/48/game-boy-advance.png'
-];
+const CACHE_NAME = 'florida-v2'; // Cambié a V2 para forzar actualización
 
 self.addEventListener('install', e => {
     e.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-        .then(() => self.skipWaiting())
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll([
+                './TiendaVirtual.html',
+                'https://img.icons8.com/color/48/mame.png'
+            ]).catch(err => console.log("Fallo al guardar: ", err));
+        })
     );
-});
-
-self.addEventListener('activate', e => {
-    e.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', e => {
     e.respondWith(
-        fetch(e.request).catch(() => caches.match(e.request))
+        fetch(e.request).catch(() => {
+            return caches.match(e.request);
+        })
+    );
+});
+
+// Esto limpia cachés viejas automáticamente al activar
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.map(key => {
+                    if (key !== CACHE_NAME) return caches.delete(key);
+                })
+            );
+        })
     );
 });
